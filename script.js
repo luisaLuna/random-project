3ebcf9dc9d9fbc6da70e6e658ab5d6d63976ecce
luisaLuna <luisacasaslu@gmail.com>
2021-05-07 16:36:55 -0500

    Javascript updates

---
+++ script.js
@@ -0,0 +1,24 @@
+let counterContainer = document.querySelector(".counter-container");
+let secondContainer = document.querySelector(".sec");
+let thrContainer = document.querySelector(".thr");
+let counters = document.querySelectorAll(".counter");
+
+// Adding styles with js: 
+counterContainer.style.display = "flex";
+counterContainer.style.alignItems = "center";
+counterContainer.style.flexDirection = "column";
+counterContainer.style.margin = "40px";
+secondContainer.style.display = "flex";
+secondContainer.style.alignItems = "center";
+secondContainer.style.flexDirection = "column";
+secondContainer.style.margin = "40px";
+thrContainer.style.display = "flex";
+thrContainer.style.alignItems = "center";
+thrContainer.style.flexDirection = "column";
+thrContainer.style.margin = "40px";
+
+
+
+
+
+
